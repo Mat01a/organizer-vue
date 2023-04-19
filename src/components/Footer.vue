92e52329e0927722e5279da8f1c119bf5060a16d
Mat Ol <matolszakowski@gmail.com>
2023-04-19 14:37:41 +0200

    Added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full h-16 bg-slate-800 poppins text-green-500 text-2xl">
+        <div class="text-center align-center leading-loose">
+            Organizer 2023
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
