c244adbef0d7a0efe17beeb06d6992f553e04a9e
Mat Ol <matolszakowski@gmail.com>
2023-07-05 11:02:23 +0200

    Added Login View and redirect after login.

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,20 +1,42 @@
 import { defineStore } from 'pinia'
+import instance from '../axios'
+import router from '../router'
 
 export const useUserStore = defineStore('user', {
     state: () => {
         return {
             darkMode: true,
+            user: null
         }
     },
     actions: {
         changeDarkMode() {
+
+        },
+        async login(form) {
+            await instance.get('/sanctum/csrf-cookie')
+            const login = await instance.post('/login', {
+                email: form.value.email,
+                password: form.value.password
+            })
+            .then((response) => {
+                this.user = {
+                    email: form.value.email
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
 
         }
     },
     getters: {
         isLoggedIn()
         {
-            return (this.email ? true : false)
+            if (this.user == null || this.user.email == undefined)
+                return false
+            else 
+                return true
         }
     }
 })
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="w-full h-screen dark:bg-slate-600">
+    <div class="w-full h-screen dark:bg-slate-500">
         <div class="absolute inset-0 mx-auto my-auto w-min h-min">
 
         </div>
         <div class="inset-0 mx-auto my-auto absolute w-full max-w-md h-min">
             <!-- Logo -->
             <IconOrganizer classes="w-24 h-24 mx-auto pb-8 drop-shadow-xl"/>
-            <div class="w-full h-full dark:bg-slate-500 rounded-2xl lato md:text-2xl dark:text-slate-200 p-12 drop-shadow-xl">
+            <div class="w-full h-full dark:bg-slate-700 rounded-2xl lato md:text-2xl dark:text-slate-200 p-12 drop-shadow-xl">
 
                 <form @submit.prevent="onSubmit">
                     <div class="w-10/12 mx-auto">
                         <label for="email" class="block">
                             E-mail
                         </label>
-                        <input type="email" id="email" class="text-xl md:mt-4 rounded-md w-full bg-slate-400 mb-8 md:p-2 dark:focus:outline-none drop-shadow-xl" v-model="form.email"/>
+                        <input type="email" id="email" class="text-xl md:mt-4 rounded-md w-full bg-slate-500 mb-8 md:p-2 dark:focus:outline-none drop-shadow-xl" v-model="form.email" required/>
                         
                         <label for="password" class="block">
                             Password
                         </label>
-                        <input type="password" id="password" class="text-xl md:mt-4 rounded-md w-full bg-slate-400 mb-8 md:p-2 dark:focus:outline-none drop-shadow-xl" v-model="form.password"/>
+                        <input type="password" id="password" class="text-xl md:mt-4 rounded-md w-full bg-slate-500 mb-8 md:p-2 dark:focus:outline-none drop-shadow-xl" v-model="form.password" required/>
                     </div>
                     
                     <div class="grid grid-cols-2 w-10/12 mx-auto gap-4">
@@ -38,6 +38,7 @@
 <script setup>
 import IconOrganizer from '../components/icons/IconOrganizer.vue';
 import { useUserStore } from '@/stores/user'
+import router from '../router'
 import { ref } from 'vue'
 
 const user = useUserStore()
@@ -48,6 +49,7 @@ const form = ref({
 
 async function onSubmit()
 {
-    user.login(form)
+    await user.login(form)
+    router.push('/projects')
 }
 </script>
