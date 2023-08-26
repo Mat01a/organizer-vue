1e2628b0342b7ba0c0cf0246bd932cbd89bc7d12
Mat Ol <matolszakowski@gmail.com>
2023-08-26 14:31:43 +0200

    Added Dropdown menu with logout function and ProjectView redirect.

---
+++ src/components/icons/IconProjects.vue
@@ -0,0 +1,7 @@
+<template>
+<svg xmlns="http://www.w3.org/2000/svg" height="48" class="fill-custom-slate-350 w-6 h-6 mx-1 float-left" viewBox="0 -960 960 960" width="48"><path d="M120-240v-60h480v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>
+</template>
+
+<script setup>
+
+</script>
